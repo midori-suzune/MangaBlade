7e2e9aa2edcc36b00f4019bd8f640a0f68c94081
Tran Viet Anh <alextran12004@gmail.com>
2026-07-09 00:02:28 +0700

    fix login

--- backend/src/main/java/com/mangablade/backend/controllers/AuthController.java
+++ backend/src/main/java/com/mangablade/backend/controllers/AuthController.java
@@ -1,14 +1,19 @@
 package com.mangablade.backend.controllers;
 
-import com.mangablade.backend.dtos.request.LoginRequest;
-import com.mangablade.backend.dtos.response.AuthResponse;
-import com.mangablade.backend.sercurity.JwtTokenProvider;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mangablade.backend.dtos.request.LoginRequest;
+import com.mangablade.backend.dtos.response.AuthResponse;
+import com.mangablade.backend.sercurity.JwtTokenProvider;
 
 @RestController
 @RequestMapping("/api/auth")
--- backend/src/main/java/com/mangablade/backend/sercurity/CustomUserDetailsService.java
+++ backend/src/main/java/com/mangablade/backend/sercurity/CustomUserDetailsService.java
@@ -1,15 +1,14 @@
 package com.mangablade.backend.sercurity;
 
-import java.util.Collections;
-
+import com.mangablade.backend.entities.User;
+import com.mangablade.backend.repositories.UserRepository;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import com.mangablade.backend.entities.User;
-import com.mangablade.backend.repositories.UserRepository;
+import java.util.Collections;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
